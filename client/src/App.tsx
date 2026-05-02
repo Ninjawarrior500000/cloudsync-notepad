8853fac755851ca0bd8100d7b84613e6b96b865b
Sohail Shaikh <isaart4110@gmail.com>
2026-05-03 03:06:15 +0530

    Update API URL to Render backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = "http://localhost:5000/api";
+const API = "https://cloudsync-notepad-backend.onrender.com/api";
 
 interface Note {
   _id: string;
